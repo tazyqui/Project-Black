d57eca719a772a764372c8fbf631254f3a43a75c
Tan Nguyen <tannguyen7070@gmail.com>
2022-09-29 22:11:22 -0400

    Update CameraMovement.cs
    Added mouse pan to camera movement script. Cleaned up the script to make it neater.

--- Assets/Scripts/Starter Scripts/Utility/CameraMovement.cs
+++ Assets/Scripts/Starter Scripts/Utility/CameraMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
@@ -22,6 +23,13 @@ public class CameraMovement : MonoBehaviour
     [Tooltip("This is the speed at which the camera follows the player, the smaller the value, the faster the movement")]
     public float smoothSpeed = 0.125f; //The bigger the value, the faster the smooth
 
+    [Space]
+    [Header("Camera Mouse Pan")]
+    [Tooltip("Check to enable mouse pan. (Camera follow mouse)")]
+    public bool mousePan = false;
+    [Tooltip("Speed of mouse pan. Greater speed will result in faster and farther camera pan")]
+    public float mousePanSpeed = 0.25f;
+
 
     private Vector3 Velocity = Vector3.zero;//This is just something we use as a reference for the smoothdamp
     private Camera camera;//This is the camera we using, it's gonna be handled in start
@@ -40,22 +48,61 @@ public class CameraMovement : MonoBehaviour
     void Update()
     {
 
-        Vector3 point = camera.WorldToViewportPoint(PlayerCharacter.position);//Grab where the player is
-        Vector3 delta = PlayerCharacter.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //We have the vector between the two
-        Vector3 destination = transform.position + delta;//And we have where we need to go
-        transform.position = Vector3.SmoothDamp(transform.position, destination, ref Velocity, smoothSpeed);//So now we make that look smooth
-
-        Transform camerATransform = camera.gameObject.transform;//We now are gonna check to see if the camera is within it's bounds
+        if (mousePan)
+        {
+            HandleMousePan();
+        }
+        else 
+        {
+            HandleCameraFollow();   //no mouse pan
+        }
 
         //Clamp the player between some bounds
         if (clampCamera)
         {
-            if (camerATransform.position.x > MaxXBoundary || camerATransform.position.x < MinXBoundary || camerATransform.position.y > MaxYBoundary || camerATransform.position.y < MinYBoundary)
-            {
-                float x = Mathf.Clamp(PlayerCharacter.position.x, MinXBoundary, MaxXBoundary);
-                float y = Mathf.Clamp(PlayerCharacter.position.y, MinYBoundary, MaxYBoundary);
-                transform.position = new Vector3(x, y, gameObject.transform.position.z);
-            }
+            HandleClampCamera();
+        }
+    }
+
+    private void HandleMousePan() 
+    {
+        Vector3 playerPosition = PlayerCharacter.position;  //get player position in world space
+        Vector3 mousePosition = GetMouseWorldPosition(Input.mousePosition, Camera.main);    //get mouse position in world space 
+        Vector3 mouseDirection = (mousePosition - playerPosition).normalized;   //get the normalized vector direction from player to mouse
+        float mouseDistance = Vector3.Distance(playerPosition, mousePosition);  // get the magnitude between player position and mouse postion
+        
+        Vector3 cameraFollowPosition = playerPosition + mouseDirection * (mouseDistance * mousePanSpeed);   //calculate the position of the camera (center)
+        cameraFollowPosition.z = -10f;  //z position of camera. Need z position to be constant in 2D
+
+        transform.position = Vector3.SmoothDamp(transform.position, cameraFollowPosition, ref Velocity, smoothSpeed);   //So now we make that look smooth
+
+    }
+
+    
+    private Vector3 GetMouseWorldPosition(Vector3 ScreenPosition, Camera worldCamera) //convert mouse position from screen space to world space
+    {
+        Vector3 vec = worldCamera.ScreenToWorldPoint(ScreenPosition);
+        vec.z = 0f;
+        return vec;
+    }
+
+    private void HandleCameraFollow() 
+    {
+        Vector3 point = camera.WorldToViewportPoint(PlayerCharacter.position);  //Grab where the player is
+        Vector3 delta = PlayerCharacter.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));   //We have the vector between the two
+        Vector3 destination = transform.position + delta;   //And we have where we need to go
+
+        transform.position = Vector3.SmoothDamp(transform.position, destination, ref Velocity, smoothSpeed);    //So now we make that look smooth
+    }
+
+    private void HandleClampCamera() 
+    {
+        Transform camerATransform = camera.gameObject.transform;    //We now are gonna check to see if the camera is within it's bounds
+        if (camerATransform.position.x > MaxXBoundary || camerATransform.position.x < MinXBoundary || camerATransform.position.y > MaxYBoundary || camerATransform.position.y < MinYBoundary)
+        {
+            float x = Mathf.Clamp(PlayerCharacter.position.x, MinXBoundary, MaxXBoundary);
+            float y = Mathf.Clamp(PlayerCharacter.position.y, MinYBoundary, MaxYBoundary);
+            transform.position = new Vector3(x, y, gameObject.transform.position.z);
         }
     }
 }
