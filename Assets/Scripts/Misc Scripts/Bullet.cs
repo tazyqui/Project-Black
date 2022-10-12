de34dabbd29c6742fc8a8c86bcdf7eb30f09e45d
ljlfun <49410924+ljlfun@users.noreply.github.com>
2022-10-12 18:56:30 -0400

    Skeeter/Bullet Changes
    Made Skeeter prefab
    Fixed bullet script (I think)
    Made bullets deal damage

--- Assets/Scripts/Misc Scripts/Bullet.cs
+++ Assets/Scripts/Misc Scripts/Bullet.cs
@@ -7,7 +7,7 @@ using UnityEngine.UIElements;
 public class Bullet : MonoBehaviour
 {
     private Vector3 shootDir;
-    public void Setup(Vector3 shootDir) 
+    public void Setup(Vector3 shootDir)
     {
         this.shootDir = shootDir.normalized;
         float angle = Mathf.Atan2(shootDir.y, shootDir.x) * Mathf.Rad2Deg;
@@ -31,11 +31,11 @@ public class Bullet : MonoBehaviour
         GameObject enemy_test = GameObject.FindGameObjectWithTag("Enemy");
         Collider2D enemy_collider = enemy_test.GetComponent<Collider2D>();
 
-        if (enemy_test != null) 
+        if (enemy_test != null)
         {
             //Hit a target
-            //enemy_test.GetComponent<EnemyHealth>.DecreaseHealth(5);
-            
+            enemy_test.GetComponent<EnemyHealth>().DecreaseHealth(5);
+
         }
 
     }
